fd97ada1fb166e5994b56baf04bae7f5ea23e3c6
Gordana Ispirova <166062997+gispirova@users.noreply.github.com>
2024-06-07 16:01:56 -0400

    Updated init.py

--- CPIExtract/utils/__init__.py
+++ CPIExtract/utils/__init__.py
@@ -1,3 +1,4 @@
 from .identifiers import compound_identifiers, protein_identifiers
+from .helper import generate_subsets, Singleton
 
-# __all__ = ['compound_identifiers', 'protein_identifiers']
+__all__ = ['generate_subsets', 'Singleton', 'compound_identifiers', 'protein_identifiers']
