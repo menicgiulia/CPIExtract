0adc84563dc2ad43ac342128c03b07e17a12aa97
Gordana Ispirova <166062997+gispirova@users.noreply.github.com>
2024-06-07 16:00:34 -0400

    Updated init.py

--- CPIExtract/pipelines/__init__.py
+++ CPIExtract/pipelines/__init__.py
@@ -1,4 +1,5 @@
 from .Comp2Prot import Comp2Prot
 from .Prot2Comp import Prot2Comp
+from .Pipeline import Pipeline
 
-__all__ = ['Comp2Prot', 'Prot2Comp']
+__all__ = ['Comp2Prot', 'Prot2Comp','Pipeline']
