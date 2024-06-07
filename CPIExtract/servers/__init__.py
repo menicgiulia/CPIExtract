544b17bd38eb7ed5a65fc62a82cbe45cc385a64e
Gordana Ispirova <166062997+gispirova@users.noreply.github.com>
2024-06-07 16:01:03 -0400

    Adding init.py

---
+++ CPIExtract/servers/__init__.py
@@ -0,0 +1,6 @@
+
+# from .BiomartServer import BiomartServer
+# from .ChEMBLServer import ChEMBLServer
+# from .PubchemServer import PubchemServer
+
+# __all__ = ['BiomartServer', 'ChEMBLServer', 'PubchemServer']
