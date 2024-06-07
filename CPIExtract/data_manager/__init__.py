379e72571af4b4c79b32dc68cf64f0bb63e27f34
Gordana Ispirova <166062997+gispirova@users.noreply.github.com>
2024-06-07 15:59:16 -0400

    Updated init.py

--- CPIExtract/data_manager/__init__.py
+++ CPIExtract/data_manager/__init__.py
@@ -1,6 +1,6 @@
-# from .DataManager import DataManager
+from .APIManager import APIManager
+from .DataManager import DataManager
 from .LocalManager import LocalManager
 from .SQLManager import SQLManager
-from .APIManager import APIManager
 
-# __all__ = ['APIManager', 'LocalManager', 'SQLManager']
+__all__ = ['APIManager', 'DataManager', 'LocalManager', 'SQLManager']
