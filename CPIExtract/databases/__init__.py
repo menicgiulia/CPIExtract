9e39a53a479ded41e0fe7d3f1578ca7a7ec26991
Gordana Ispirova <166062997+gispirova@users.noreply.github.com>
2024-06-07 16:00:06 -0400

    Add files via upload

--- CPIExtract/databases/__init__.py
+++ CPIExtract/databases/__init__.py
@@ -1,11 +1,12 @@
-from .PubChem import PubChem 
-from .CTD import CTD
+from .BindingDB import BindingDB
 from .ChEMBL import ChEMBL
+from .CTD import CTD
+from .Database import Database
 from .DrugBank import DB
+from .DrugCentral import DrugCentral
 from .DTC import DTC
 from .OTP import OTP
+from .PubChem import PubChem
 from .Stitch import Stitch
-from .BindingDB import BindingDB
-from .DrugCentral import DrugCentral
 
-__all__ = ['PubChem', 'CTD', 'ChEMBL', 'DB', 'DTC', 'OTP', 'Stitch', 'BindingDB', 'DrugCentral']
+__all__ = ['BindingDB', 'ChEMBL', 'CTD', 'Database', 'DB', 'DrugCentral', 'DTC', 'OTP', 'PubChem', 'Stitch']
